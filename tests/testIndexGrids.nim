6bb3362c73d8b37d02a0c2ad10cb1e553089bcd8
Mason McGill <mason.b.mcgill@gmail.com>
2015-04-22 17:11:03 -0400

    Added grid index tests

--- tests/testIndexGrids.nim
+++ tests/testIndexGrids.nim
@@ -1,16 +1,38 @@
 import unittest
 import numerics
 
-test "grid.indices":
+test "indices(size)":
   block:
     var i = 0
-    for e in @@["0", "1", "2"].indices:
+    var g = indices([3])
+    check g.size == [3]
+    for e in g:
       check e == [i]
       i += 1
     check i == 3
   block:
     var i = 0
-    for e in @@[["0", "1"], ["2", "3"]].indices:
+    var g = indices([2, 2])
+    check g.size == [2, 2]
+    for e in g:
+      check e == [i div 2, i mod 2]
+      i += 1
+    check i == 4
+
+test "indices(grid)":
+  block:
+    var i = 0
+    var g = indices(@@["0", "1", "2"])
+    check g.size == [3]
+    for e in g:
+      check e == [i]
+      i += 1
+    check i == 3
+  block:
+    var i = 0
+    var g = indices(@@[["0", "1"], ["2", "3"]])
+    check g.size == [2, 2]
+    for e in g:
       check e == [i div 2, i mod 2]
       i += 1
     check i == 4
